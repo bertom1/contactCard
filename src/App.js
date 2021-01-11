d7ef9a8ecdc618c1fcd452ee517645b9140d5695
Roberto C Melchor <rmelchor151@gmail.com>
2021-01-11 10:28:57 -0500

    remove unused code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react'
 import ContactCard from './contactCard.jsx'
-import IncreaseDecrease from './increaseDecrease.jsx'
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
         <ContactCard name="Taylor" email = "taylor@domain" phone = {12222} workphone = {12345} />
         <ContactCard name="roberto" email = "roberto@domain" phone = {11111} />
         <ContactCard name="dave" email = "dave@domain" phone = {13333} />
-        <IncreaseDecrease num = {3} />
       </header>
     </div>
   );
